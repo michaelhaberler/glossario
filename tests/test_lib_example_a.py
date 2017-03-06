1d90911c06041c7ab7eb774922db699cb91f9404
Michael Haberler <michael.haberler@gmx.at>
2017-03-06 21:46:33 +0100

    Use import alias in test code

--- tests/test_lib_example_a.py
+++ tests/test_lib_example_a.py
@@ -1,7 +1,11 @@
 
 from lib_example import lib_example_a
+from lib_example.lib_example_a import B as BE
 
 
 def test_A():
     a = lib_example_a.A("test_A")
     assert a.name == "test_A"
+
+def test_BE():
+    be = BE('be')
